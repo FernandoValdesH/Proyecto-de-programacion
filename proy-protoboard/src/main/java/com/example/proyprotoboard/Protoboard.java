b12f4edacbd51eba38fedb9bd764c7d1e3ec9a62
Tomas Corvalán <tomascorvalancsm@gmail.com>
2024-09-09 01:13:51 -0300

    Delete proy-protoboard directory

--- proy-protoboard/src/main/java/com/example/proyprotoboard/Protoboard.java
+++
@@ -1,9 +0,0 @@
-package com.example.proyprotoboard;
-
-public class Protoboard {
-
-    private String[][] base_protoboard;
-    private Cable cable_positivo;
-    private Cable cable_negativo;
-
-}
