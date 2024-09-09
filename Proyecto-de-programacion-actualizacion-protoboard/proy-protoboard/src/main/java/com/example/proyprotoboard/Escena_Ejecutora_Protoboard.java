ec34198face06f3f16a61ccb53b437b3e8cd0318
FernandoValdesH <128178390+FernandoValdesH@users.noreply.github.com>
2024-09-09 01:30:01 -0300

    Add files via upload

---
+++ Proyecto-de-programacion-actualizacion-protoboard/proy-protoboard/src/main/java/com/example/proyprotoboard/Escena_Ejecutora_Protoboard.java
@@ -0,0 +1,29 @@
+package com.example.proyprotoboard;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Escena_Ejecutora_Protoboard extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Escena_Ejecutora_Protoboard.class.getResource("Escena_Protoboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
+        stage.setScene(scene);
+        stage.show();
+
+
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+
+    }
+}
---
+++ Proyecto-de-programacion-actualizacion-protoboard/proy-protoboard/src/main/java/com/example/proyprotoboard/Led.java
@@ -0,0 +1,9 @@
+package com.example.proyprotoboard;
+public class Led extends Indicador{
+    Boolean encendido;
+    public Led(){
+        super();
+        this.encendido=false;
+    }
+
+}
