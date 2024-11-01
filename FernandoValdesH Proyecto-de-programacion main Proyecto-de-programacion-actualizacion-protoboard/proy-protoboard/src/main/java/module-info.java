6ea690980418bfcf26fa5200fbb82c583aa72cf0
FernandoValdesH <128178390+FernandoValdesH@users.noreply.github.com>
2024-11-01 20:11:24 -0300

    Delete FernandoValdesH Proyecto-de-programacion main Proyecto-de-programacion-actualizacion-protoboard/proy-protoboard directory

--- FernandoValdesH Proyecto-de-programacion main Proyecto-de-programacion-actualizacion-protoboard/proy-protoboard/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.proyprotoboard {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.proyprotoboard to javafx.fxml;
-    exports com.example.proyprotoboard;
-}
